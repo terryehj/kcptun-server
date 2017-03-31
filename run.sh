468a875de628b21b59cd5dffee9a19697cb161f9
Terry Yi <terry.ehj@gmail.com>
2017-03-31 18:19:39 +0800

    improved

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cd `dirname $0`
-eval $(ps -ef | grep server_linux_amd64 | grep -v grep | awk '{print "kill "$2}')
-nohup ./server_linux_amd64 -c config.json >> /dev/null 2>&1 &
+eval $(ps -ef | grep server_linux_amd64 | grep -v grep | awk '{print $2}' | xargs kill -9
+./server_linux_amd64 -c config.json &> kcptun.log &
--- stop.sh
+++ stop.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-eval $(ps -ef | grep server_linux_amd64 | grep -v grep | awk '{print "kill "$2}')
+eval $(ps -ef | grep server_linux_amd64 | grep -v grep | awk '{print $2}' | xargs kill -9
