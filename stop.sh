e22cd1a5b9cbac896b340fe964b12716b9c8f8c6
Terry Yi <terry.ehj@gmail.com>
2017-03-31 18:32:01 +0800

    1

--- stop.sh
+++ stop.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-eval $(ps -ef | grep server_linux_amd64 | grep -v grep | awk '{print $2}' | xargs kill -9
+ps -ef | grep server_linux_amd64 | grep -v grep | awk '{print $2}' | xargs kill -9
